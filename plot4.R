accb6f20d34bc05b82cce01537337e77d39d172c
Cindy Wong <cindy.wong@genesys.com>
2017-05-29 19:36:18 -0400

    Getting around plotting directly to png() doesn't create 2x2 plots
    For some reason plotting directly to png() device only saves the last
    plot in the file.  Got around it using dev.copy().  Not the neatest
    solution, but it works.

--- plot4.R
+++ plot4.R
@@ -5,7 +5,7 @@ source('./getdata.R')
 par(mfrow = c(2,2))
 
 # Plots to plot4.png
-png("plot4.png")
+# png("plot4.png")  <-- this doesn't create 2x2 plots for some reason; using dev.copy() below
 
 # First plot: global active power across time
 plot(subdata$DateTime, subdata$Global_active_power, xlab="", ylab="Global Active Power", type="n")
@@ -28,5 +28,7 @@ legend("topright", lty=c(1,1,1), col=c("black", "red", "blue"), legend=c("Sub_me
 plot(subdata$DateTime, subdata$Global_reactive_power, xlab="datatime", ylab="Global_reactive_power", type="n")
 lines(subdata$DateTime, subdata$Global_reactive_power, col="black")
 
+# plotting directly to png() only seems to create the last plot; using dev.copy() to get around it
+dev.copy(png, "plot4.png")
 dev.off()
 
